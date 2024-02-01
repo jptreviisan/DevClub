4b2d3fa95a6e1d0e08fe14872d9413e31f841643
João Pedro Trevisan <145926289+jptreviisan@users.noreply.github.com>
2024-02-01 13:04:32 -0300

    Configurei o botao

--- Conversor/script.js
+++ Conversor/script.js
@@ -1,7 +1,11 @@
 const convertButton = document.querySelector(".convert-button");
 
 function convertValues(){
-    console.log("Funcionou");
+    const inputCurrencyValue = document.querySelector(".input-currency").value
+    const dolarToday = 5.2
+    const convertedValue = inputCurrencyValue / dolarToday
+
+    console.log(convertedValue)
 }
 
 convertButton.addEventListener("click", convertValues);
