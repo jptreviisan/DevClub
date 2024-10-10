58ad2bd331da8ae83759c87a4548b7068a381380
João Pedro Trevisan <joao.treviisan@outlook.com>
2024-10-09 23:51:13 -0300

    Projeto Pt4

---
+++ desafio_Map/desafio.js
@@ -0,0 +1,21 @@
+
+const companie = [
+    { name: 'Samsung', marketValue: 50, CEO: 'Kin Hyun Suk', foundedOn: 1938 },
+    { name: 'Microsoft', marketValue: 415, CEO: 'Satya', foundedOn: 1975 },
+    { name: 'Intel', marketValue: 117, CEO: 'Bryan', foundedOn: 1968 },
+    { name: 'Facebook', marketValue: 383, CEO: 'Mark', foundedOn: 2004 },
+    { name: 'Spotify', marketValue: 30, CEO: 'Daniel', foundedOn: 2006 },
+    { name: 'Apple', marketValue: 845, CEO: 'Tim', foundedOn: 1976 },
+]
+
+const sub10Percent = company => {
+    company.marketValue = company.marketValue - (company.marketValue / 10)
+
+    return company
+}
+const filterCompanies = company => company.foundedOn > 1980
+const calculateTotalMarketValue = (acc, company) => acc + company.marketValue
+
+const newCompanie = companie.map(sub10Percent).filter(filterCompanies).reduce(calculateTotalMarketValue, 0)
+
+console.log(newCompanie)
